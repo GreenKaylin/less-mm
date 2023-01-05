623b60151798067d42d326372d92ce18febf877a
Ellen <82329170+GreenKaylin@users.noreply.github.com>
2023-01-05 12:47:21 +0000

    Add files via upload

---
+++ xray.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+UUID=f81cb6ea-dee5-41a1-a6d2-4c875d04fddf
+
+# Set config.json
+sed -i "s/PORT/$PORT/g" /etc/xray/config.json
+sed -i "s/UUID/$UUID/g" /etc/xray/config.json
+
+echo starting xray platform
+echo starting with UUID:$UUID
+echo listening at 0.0.0.0:$PORT
+
+exec "$@"
+
